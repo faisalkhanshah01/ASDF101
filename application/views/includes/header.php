5003511a3bb46277b88e292433a4f285353b88eb
root <root@example.com>
2018-10-26 00:26:33 +0530

    jquery multi upload ver 1.0

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -5,19 +5,32 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="copyright" content="Copyright <?php echo date('Y');?> Flash On Mind, All Copyrights Reserved"/>
-<meta name="designer" content="sachintyagi - Flash On Mind : shakti.singh@flashonmind .com"/> 
+<meta name="designer" content="sachintyagi - Flash On Mind : shakti.singh@flashonmind .com"/>
 <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 <meta name="description" content="">
 <meta name="author" content="">
-	
+
 	<script type="text/javascript" src="<?php echo $includes_dir;?>js/jquery.min.js"></script>
 	<link rel="stylesheet" href="<?php echo $includes_dir;?>css/bootstrap.min.css">
 	<link rel="stylesheet" href="<?php echo $includes_dir;?>css/dataTables.min.css">
 	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
 	<link rel="stylesheet" href="https://cdn.datatables.net/1.10.10/css/jquery.dataTables.min.css">
+	<?php
+	$arr = explode("/",$_SERVER['REQUEST_URI']);
+	if($arr[sizeof($arr)-2]==='pdm'){
+		?>
+		<link rel="stylesheet" href="<?php echo $includes_dir;?>fileupload/css/jquery.fileupload.css">
+		<link rel="stylesheet" href="<?php echo $includes_dir;?>fileupload/css/jquery.fileupload-ui.css">
+		<!-- CSS adjustments for browsers with JavaScript disabled -->
+		<noscript><link rel="stylesheet" href="<?php echo $includes_dir;?>fileupload/css/jquery.fileupload-noscript.css"></noscript>
+		<noscript><link rel="stylesheet" href="<?php echo $includes_dir;?>fileupload/css/jquery.fileupload-ui-noscript.css"></noscript>
+		<?php
+	}
+	?>
+
 	<link rel="stylesheet" href="<?php echo $includes_dir;?>css/custom.css">
 	<link rel="stylesheet" href="<?php echo $includes_dir; ?>css/style23.css">
-	
+
 	<!-- <link rel="stylesheet" href="<?php //echo $includes_dir;?>css/structure.css?v=1.0">-->
 <title>Kare	</title>
 <script type="text/javascript">
@@ -29,7 +42,7 @@
 }
 .infonet_header, .data_on_demand li{
 	margin: 5px 0px 5px 0px;
-	
+
 }
 
 .copyrights{
@@ -55,12 +68,12 @@
 }
 
 .infonet_header_search_form{
-	width:550px; 
-	margin-bottom:5px; 
+	width:550px;
+	margin-bottom:5px;
 	margin-top:-18px;
 	margin-left:45%
 }
-.input_search{	
+.input_search{
 width:51%;
 }
 
@@ -87,7 +100,7 @@ width:51%;
                  $('#global_search').keyup(function() {
 			$("#global_searchViewShow").show();
 			$("#global_searchAllView").hide();
-			
+
 			var data = {
 				product:$("#global_search").val(),
 				checkProduct:1,
@@ -108,34 +121,34 @@ width:51%;
 						$('#global_search_view').html(output.message);
 					}else if(output.responseType == 'fail_url'){
 						window.location.href = $(location).attr("href");
-					}	
+					}
 				}
-			});  
-			return false; 
+			});
+			return false;
 		});
-	});	
+	});
 </script>
 </head>
 
 <div id="loading" style="display:none"><center><h2><font color="white">Please wait file is getting ready for download..</font></h2></center></div>
 <body>
-	
+
 	<div class="container-fluid"> <!--  Container starting here and ending in footer.php  -->
 			<div class="row">
 				<div class="col-md-12">
 					<?php
-						
-						if( isset($this->session->flexi_auth['group'])){ $group = array_keys($this->session->flexi_auth['group']); }	
+
+						if( isset($this->session->flexi_auth['group'])){ $group = array_keys($this->session->flexi_auth['group']); }
 						if(empty($group[0])){
 					?>
 							<div style="float:right;padding-top:40px;">
-									<a href="<?php echo $this->config->item('knowledgetree_url');?>/Infonet" class="btn btn-primary act-but-prm" role="button" style="background-color: <?php echo $_SESSION['client']['color_code'];?>">knowledgetree Login</a> 
+									<a href="<?php echo $this->config->item('knowledgetree_url');?>/Infonet" class="btn btn-primary act-but-prm" role="button" style="background-color: <?php echo $_SESSION['client']['color_code'];?>">knowledgetree Login</a>
 							</div>
-					<?php }?>	
-					<?php 
+					<?php }?>
+					<?php
 					   // $kdvalue = array_values($this->session->flexi_auth['group']);
-						
-						if( isset($this->session->flexi_auth['group'])){ $kdkey = array_keys($this->session->flexi_auth['group']); }	
+
+						if( isset($this->session->flexi_auth['group'])){ $kdkey = array_keys($this->session->flexi_auth['group']); }
 						if(isset($kdkey[0]) && (($kdkey[0] == 10) || ($kdkey[0] == 11))){
 					?>
 							<div class="col-md-4">
@@ -154,7 +167,7 @@ width:51%;
 											<input type="text" class="form-control" name="search_product" id="search_product" placeholder="Search for...">
 											<span class="input-group-addon">
 													<i class="glyphicon glyphicon-search"></i>
-											</span> 
+											</span>
 										</div>
 									</div>
 								</div>
@@ -181,18 +194,17 @@ width:51%;
 									<?php } ?>
 					<?php }?>
 
-					<div class="col-md-2 pull-right">	
+					<div class="col-md-2 pull-right">
 					<?php if($this->session->userdata('userImage')){
 						$fullName = $this->session->userdata('firstName').' '.$this->session->userdata('lastName');
 					?>
 						<a class="navbar-brand pull-right hidden-xs" href="<?php echo $base_url;?>auth_public/update_account"> <img title="<?php echo $fullName; ?>" class="userImage" src="<?php echo $base_url;?>uploads/images/users/<?php echo $this->session->userdata('userImage'); ?>" width="80" height='70' alt="<?php echo $fullName; ?>" ></a>
-					<?php }else if($this->session->userdata('firstName') !=''){ 
+					<?php }else if($this->session->userdata('firstName') !=''){
 						$fullName = $this->session->userdata('firstName').' '.$this->session->userdata('lastName');
 					?>
 						<a id="popover" class="navbar-brand pull-right hidden-xs" href="<?php echo $base_url;?>auth_public/update_account"> <img title="<?php echo $fullName; ?>" class="userImage" src="<?php echo $base_url;?>uploads/images/users/defult_image.jpg" width="80" height='70' alt="<?php echo $fullName; ?>" ></a>
-						
+
 				<?php	} ?>
 					</div>
 				</div>
-			</div> 
-			
+			</div>
--- application/views/userpanel/pdm/index.php
+++ application/views/userpanel/pdm/index.php
@@ -2,6 +2,7 @@
 <!-- Navigation -->
 <?php $this->load->view('includes/head'); ?>
 
+
 <!-- Pdm content starts here -->
 <div class="row" class="msg-display">
     <div class="col-md-12">
@@ -30,10 +31,12 @@
                         ?></a></li>
             </ul>
             <!-- Tab panes -->
+
             <div class="tab-content">
                 <div role="tabpanel" class="tab-pane active" id="mdata_form">
-                            <?php echo form_open_multipart(current_url(), 'class="master_data_form"'); ?>
-                    <div class="col-md-12">
+                            <?php echo form_open_multipart(current_url(), 'id="fileupload" class="master_data_form"'); ?>
+
+                        <div class="col-md-12">
                         <legend  class="home-heading"><?php
                             if ($lang["add_site_id_data"]['description'] != '') {
                                 echo $lang["add_site_id_data"]['description'];
@@ -75,7 +78,7 @@
                                     <div class="panel-heading">
                                       <span class="step-label">Step No.1</span>
                                       <div class="pull-right">
-                                      <i class="glyphicon glyphicon-trash"></i>&nbsp;    
+                                      <i class="glyphicon glyphicon-trash"></i>&nbsp;
                                       <i id="clone-1" onclick="clonebox(this)" class="mr10 cloneicon glyphicon glyphicon-duplicate"></i>
                                       <i id="close-1" onclick="collapsebox(this)" class="collapseicon pull-right glyphicon glyphicon-chevron-up"></i>
                                       </div>
@@ -124,7 +127,45 @@
                                               <label for="group" class="control-label">Upload Media (Video Url )</label>
                                               <input type="text" id="pdm_media" class="form-control" name="pdm_media[]" id="pdm_media" />
                                           </div>
-                                        
+                                          <!-- file upload feature starts-->
+                                          <div class="form-group col-md-12">
+                                            <div class="row fileupload-buttonbar">
+                                                        <div class="col-lg-7">
+                                                            <!-- The fileinput-button span is used to style the file input field as button -->
+                                                            <span class="btn btn-success fileinput-button">
+                                                                <i class="glyphicon glyphicon-plus"></i>
+                                                                <span>Add files...</span>
+                                                                <input type="file" name="files[]" multiple>
+                                                            </span>
+                                                            <button type="submit" class="btn btn-primary start">
+                                                                <i class="glyphicon glyphicon-upload"></i>
+                                                                <span>Start upload</span>
+                                                            </button>
+                                                            <button type="reset" class="btn btn-warning cancel">
+                                                                <i class="glyphicon glyphicon-ban-circle"></i>
+                                                                <span>Cancel upload</span>
+                                                            </button>
+                                                            <!-- <button type="button" class="btn btn-danger delete">
+                                                                <i class="glyphicon glyphicon-trash"></i>
+                                                                <span>Delete</span>
+                                                            </button> -->
+                                                            <!-- The global file processing state -->
+                                                            <span class="fileupload-process"></span>
+                                                        </div>
+                                                        <!-- The global progress state -->
+                                                        <div class="col-lg-5 fileupload-progress fade">
+                                                            <!-- The global progress bar -->
+                                                            <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100">
+                                                                <div class="progress-bar progress-bar-success" style="width:0%;"></div>
+                                                            </div>
+                                                            <!-- The extended global progress state -->
+                                                            <div class="progress-extended">&nbsp;</div>
+                                                        </div>
+                                                    </div>
+                                                    <!-- The table listing the files available for upload/download -->
+                                                    <table role="presentation" class="table table-striped"><tbody class="files"></tbody></table>
+                                          </div>
+                                          <!-- file upload feature ends -->
 
                                       </div>
                                     </div>
@@ -136,7 +177,7 @@
                         <!-- /.section -->
                     </div>
                     <!-- /.sections -->
-                    
+
                     <div class="form-group" >
                          <div class="col-md-offset-4 col-md-8" style="margin-top:30px;">
                             <input type="hidden" name="master_id"  id="master_id" value="" />
@@ -250,3 +291,115 @@
     });
 });
     </script>
+
+    <!-- Code for jquery file upload  -->
+    <!-- The template to display files available for upload -->
+    <script id="template-upload" type="text/x-tmpl">
+    {% for (var i=0, file; file=o.files[i]; i++) { %}
+        <tr class="template-upload fade">
+            <td>
+                <span class="preview"></span>
+            </td>
+            <td>
+                <p class="name">{%=file.name%}</p>
+                <strong class="error text-danger"></strong>
+            </td>
+            <td>
+                <p class="size">Processing...</p>
+                <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0"><div class="progress-bar progress-bar-success" style="width:0%;"></div></div>
+            </td>
+            <td>
+                {% if (!i && !o.options.autoUpload) { %}
+                    <button class="btn btn-primary start" disabled>
+                        <i class="glyphicon glyphicon-upload"></i>
+                        <span>Start</span>
+                    </button>
+                {% } %}
+                {% if (!i) { %}
+                    <button class="btn btn-warning cancel">
+                        <i class="glyphicon glyphicon-ban-circle"></i>
+                        <span>Cancel</span>
+                    </button>
+                {% } %}
+            </td>
+        </tr>
+    {% } %}
+    </script>
+    <!-- The template to display files available for download -->
+    <script id="template-download" type="text/x-tmpl">
+    {% for (var i=0, file; file=o.files[i]; i++) { %}
+        <tr class="template-download fade">
+            <td>
+                <span class="preview">
+                    {% if (file.thumbnailUrl) { %}
+                        <a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" data-gallery><img src="{%=file.thumbnailUrl%}"></a>
+                    {% } %}
+                </span>
+            </td>
+            <td>
+                <p class="name">
+                    {% if (file.url) { %}
+                        <a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" {%=file.thumbnailUrl?'data-gallery':''%}>{%=file.name%}</a>
+                    {% } else { %}
+                        <span>{%=file.name%}</span>
+                    {% } %}
+                </p>
+                {% if (file.error) { %}
+                    <div><span class="label label-danger">Error</span> {%=file.error%}</div>
+                {% } %}
+            </td>
+            <td>
+                <span class="size">{%=o.formatFileSize(file.size)%}</span>
+            </td>
+            <td>
+                {% if (file.deleteUrl) { %}
+                    <button class="btn btn-danger delete" data-type="{%=file.deleteType%}" data-url="{%=file.deleteUrl%}"{% if (file.deleteWithCredentials) { %} data-xhr-fields='{"withCredentials":true}'{% } %}>
+                        <i class="glyphicon glyphicon-trash"></i>
+                        <span>Delete</span>
+                    </button>
+                    <input type="checkbox" name="delete" value="1" class="toggle">
+                {% } else { %}
+                    <button class="btn btn-warning cancel">
+                        <i class="glyphicon glyphicon-ban-circle"></i>
+                        <span>Cancel</span>
+                    </button>
+                {% } %}
+            </td>
+        </tr>
+    {% } %}
+    </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- The jQuery UI widget factory, can be omitted if jQuery UI is already included -->
+    <script src="<?php echo $includes_dir;?>fileupload/js/vendor/jquery.ui.widget.js"></script>
+    <!-- The Templates plugin is included to render the upload/download listings -->
+    <script src="https://blueimp.github.io/JavaScript-Templates/js/tmpl.min.js"></script>
+    <!-- The Load Image plugin is included for the preview images and image resizing functionality -->
+    <script src="https://blueimp.github.io/JavaScript-Load-Image/js/load-image.all.min.js"></script>
+    <!-- The Canvas to Blob plugin is included for image resizing functionality -->
+    <script src="https://blueimp.github.io/JavaScript-Canvas-to-Blob/js/canvas-to-blob.min.js"></script>
+    <!-- Bootstrap JS is not required, but included for the responsive demo navigation -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- blueimp Gallery script -->
+    <script src="https://blueimp.github.io/Gallery/js/jquery.blueimp-gallery.min.js"></script>
+    <!-- The Iframe Transport is required for browsers without support for XHR file uploads -->
+    <script src="<?php echo $includes_dir;?>fileupload/js/jquery.iframe-transport.js"></script>
+    <!-- The basic File Upload plugin -->
+    <script src="<?php echo $includes_dir;?>fileupload/js/jquery.fileupload.js"></script>
+    <!-- The File Upload processing plugin -->
+    <script src="<?php echo $includes_dir;?>fileupload/js/jquery.fileupload-process.js"></script>
+    <!-- The File Upload image preview & resize plugin -->
+    <script src="<?php echo $includes_dir;?>fileupload/js/jquery.fileupload-image.js"></script>
+    <!-- The File Upload audio preview plugin -->
+    <script src="<?php echo $includes_dir;?>fileupload/js/jquery.fileupload-audio.js"></script>
+    <!-- The File Upload video preview plugin -->
+    <script src="<?php echo $includes_dir;?>fileupload/js/jquery.fileupload-video.js"></script>
+    <!-- The File Upload validation plugin -->
+    <script src="<?php echo $includes_dir;?>fileupload/js/jquery.fileupload-validate.js"></script>
+    <!-- The File Upload user interface plugin -->
+    <script src="<?php echo $includes_dir;?>fileupload/js/jquery.fileupload-ui.js"></script>
+    <!-- The main application script -->
+    <script src="<?php echo $includes_dir;?>fileupload/js/main.js"></script>
+    <!-- The XDomainRequest Transport is included for cross-domain file deletion for IE 8 and IE 9 -->
+    <!--[if (gte IE 8)&(lt IE 10)]>
+    <script src="<?php echo $includes_dir;?>fileupload/js/cors/jquery.xdr-transport.js"></script>
+    <!-- Code for jquery file upload  ends-->
