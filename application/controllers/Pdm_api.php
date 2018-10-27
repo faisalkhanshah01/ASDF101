19db38aebaf9f33ce6341da644511c2e7cef8116
root <root@example.com>
2018-10-27 19:43:40 +0530

    submit inspection api ver 1.0

--- application/controllers/Pdm_api.php
+++ application/controllers/Pdm_api.php
@@ -1,43 +1,43 @@
 <?php
 require(APPPATH.'/libraries/REST_Controller.php');
- 
+
 class Pdm_api extends REST_Controller
 //class Asm_api extends CI_Controller
 {
     private static $ASM_PRODUCTS=null;
-            
-    #const ASM_PRODUCTS='ASDF';        
-   # const ASM_STORE_PRODUCTS=        
-   # const ASM_PRODUCTS=           
-            
+
+    #const ASM_PRODUCTS='ASDF';
+   # const ASM_STORE_PRODUCTS=
+   # const ASM_PRODUCTS=
+
     public function __construct(){
 
         parent::__construct();
         #$this->load->database('karedb');
         $this->load->model('build_model');
         $this->load->model('kare_model');
-        $this->load->helper('url'); 
+        $this->load->helper('url');
         $this->client_id=$_SESSION['client']['client_id'];
-        
+
         // PDM TABLES CONSTANT
         #self:: $ASM_PRODUCTS = "asm_{$this->client_id}_products";
     }
-    
-    
+
+
     /*function get($query){
         return $this->input->get($query);
     }*/
     /*function post($query){
         return $this->input->post($query);
     }*/
-    
+
    function index(){
         echo "index function PDM MODULE"; die;
     }
-    
-    // PDM STEPS DISPLY CREATED IN BACKEND  
+
+    // PDM STEPS DISPLY CREATED IN BACKEND
    function pdm_steps_get(){
-       
+
        $asset_code=trim($this->get('asset_code'));
        $err_params=array();
        if(!$asset_code){
@@ -45,32 +45,72 @@ class Pdm_api extends REST_Controller
         }
        $client_id=trim($this->get('client_id'));
        if(!$client_id){
-           
+
            $err_params[]='client_id';
        }
-       
+
        if(count($err_params)){
            $e_str=  implode(',', $err_params);
            $response=array('status_code'=>400,'message'=>"Bad Request- {$e_str} is missing");
        }else{
            $where=array('pdm_client_fk'=>$client_id,'pdm_asset_code'=>$asset_code);
            $steps=$this->build_model->get_data('periodic_maintenance',$where);
-           
+
            if($steps){
                $response=array('status_code'=>200,'message'=>'Steps found','data'=>$steps);
            }else{
-              $response=array('status_code'=>404,'message'=>'Steps not found','data'=>$steps); 
+              $response=array('status_code'=>404,'message'=>'Steps not found','data'=>$steps);
            }
            #echo "<pre>";
-           #print_r($steps);   
+           #print_r($steps);
       }
-      
-      $this->response($response,200);  
+
+      $this->response($response,200);
+
+   }
+
+   // Insert inspection data API
+    function submit_inspection_post(){
+      $_POST = json_decode(file_get_contents('php://input'), true);
+      if(empty($_POST['before_images']) || empty($_POST['after_images']) || empty($_POST['action_propose']) || empty($_POST['action_taken'])) {
+        $response = array(
+          'msg_code'	=> 404,
+          'msg'		=> 'Fields cannot be blank'
+        );
+      }
+      else{
+
+        $params = [
+          'before_images' => $_POST['before_images'],
+          'after_images' => $_POST['after_images'],
+          'action_propose' => trim(strip_tags($_POST['action_propose'])),
+          'action_taken' => trim(strip_tags($_POST['action_taken'])),
+        ];
+        $where=array(); //where condition if any
+        $tablename = 'xyz';
+        $query_result = $this->build_model->submit_inspection($tablename, $where, $params);
         
-   } 
-   
-   
-   
-    
+        if($query_result['affected_rows']>0){
+
+          $response = array(
+            'msg_code'	=> 200,
+            'msg'		=> 'Data updated successfully',
+            'last_id' => $query_result['last_id'],
+            'affected_rows' => $query_result['affected_rows']
+          );
+        }
+        else{
+
+          $response = array(
+            'msg_code'	=> 200,
+            'msg'		=> 'Data not Inserted',
+            'affected_rows' => $query_result['affected_rows']
+          );
+        }
+      }
+      $this->response($response,200);
+    }
+
+
 }
 ?>
--- application/models/Build_model.php
+++ application/models/Build_model.php
@@ -1,18 +1,18 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
  class Build_model extends CI_Model{
-     
+
      function __construct(){
          parent::__construct();
          //$this->db=$this->karedb;
          //print_r($this->db);
      }
-     
+
     function insert_data($table,$data){
         $id=$this->db->insert($table,$data);
         #echo $this->db->last_query();
@@ -21,18 +21,18 @@
         }else{
              echo $this->db->_error_message();
             return false;
-            
+
         }
     }
-    
+
     function insert_data_batch($table,$dbdata){
 	return $this->db->insert_batch($table,$dbdata);
 		//echo $this->db->last_query();
     }
-    
-    
+
+
     function update_table($table,$data,$where){
-        
+
         #print_r($where); //die;
         if(!count($where)){
             return false;
@@ -46,11 +46,11 @@
             if($result){
                 return $result;
             }else{
-             return false;   
+             return false;
             }
         }
     }
-    
+
     function get_data($table,$where=1,$order_str=null,$fields='*'){
         /*if(!is_array()){
         }*/
@@ -58,24 +58,24 @@
         $this->db->select($fields);
         $this->db->where($where);
         if($order_str){
-           $this->db->order_by($order_str); 
+           $this->db->order_by($order_str);
         }
-        
-        $query  = $this->db->get($table); 
+
+        $query  = $this->db->get($table);
         #echo $this->db->last_query();
-        
+
         if($query->num_rows()>1){
          return $query->result_array();
         } if($query->num_rows == 1){
               return $query->result_array();
             #return $query->row_array();
         }else{
-           
+
             return false;
         }
-       
+
     }
-    
+
     function get_data_row($table,$where=1,$order_str=null,$fields='*'){
           $data= $this->get_data($table,$where,$order_str,$fields);
           #echo $this->db->last_query();
@@ -83,90 +83,90 @@
               return $data[0];
           }else{
               return $data;
-          } 
+          }
     }
-    
-    
+
+
     function filter_data($table,$filter,$where=null){
-        
+
         if(isset($filter['distinct'])){
-            $this->db->distinct(); 
+            $this->db->distinct();
             $this->db->select($filter['distinct']);
         }
-        
+
         if(is_array($where)){
-           $this->db->where($where); 
+           $this->db->where($where);
         }
-        
+
         #$field = "DISTINCT({$filter['distinct']})";
         #$this->db->select($field);
         #$this->db->from($table);
-        
+
         $query=$this->db->get($table);
         #echo $this->db->last_query();
-        
+
         if($query->num_rows()){
             return $query->result_array();
         }else{
-            
+
             return false;
         }
-        
+
     }
-    
-    
-    
+
+
+
     function delete_record($table,$where){
         if(!count($where)){
-            
+
             return false;
-            
+
         } else{
-            
+
             $this->db->where($where);
             $result=$this->db->delete($table);
             #echo $this->db->last_query();
             if($result){
                 return true;
             }else{
-             return false;   
-            }    
-        } 
+             return false;
+            }
+        }
     }
-    
+
    function row_count($table,$where){
        $this->db->select('*');
        $this->db->where($where);
        $query=$this->db->get($table);
        #echo $this->db->last_query();
        return $query->num_rows();
-   } 
-   
-  
+   }
+
+
     function query_data($table,$where=null,$filter=null,$field=null){
-        
+
         if(isset($filter['distinct'])){
-            $this->db->distinct(); 
+            $this->db->distinct();
             $this->db->select($filter['distinct']);
         }
-        
+
         if(is_array($where)){
-           $this->db->where($where); 
+           $this->db->where($where);
         }
         $query=$this->db->get($table);
         //echo $this->db->last_query();
-        
+
         if($query->num_rows()){
             return $query->result_array();
         }else{
             return false;
         }
-        
+
     }
-    
-   
+
+
    public function android_notification($token,$title,$msg,$data=null) {
-          
+
         $fields = array (
             'to' => $token,
             'notification' => array (
@@ -193,8 +193,8 @@
         return $result = curl_exec ( $ch );
         curl_close ( $ch );
     }
-    
-    
+
+
     public function ios_notification($token, $title, $msg,$data=null) {
         $fcmMsg = array(
             'body'  => $msg,
@@ -225,7 +225,23 @@
         curl_close( $ch );
         echo $result . "\n\n";
     }
-    
-    
-   
+    function submit_inspection($table,$where=1,$params){
+
+        // $this->db->insert($table,$params);
+
+        // $response = array(
+        //   'last_id' => ($this->db->insert_id())?$this->db->insert_id():null,
+        //   'affected_rows' => ($this->db->affected_rows())?$this->db->affected_rows():0,
+        //  );
+
+        $response = array(
+          'last_id' => 123,
+          'affected_rows' => 1
+         );
+
+        return $response;
+    }
+
+
+
  }
